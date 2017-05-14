954e1aea3b59a4f6949c1f75fca07160525fde2a
Stafeyev Alexei <noegon@yandex.ru>
2017-05-14 16:48:09 +0300

    [PROJECT_CREATED]First version of 1-st task was added

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by user on 13.05.17.
+//  Copyright © 2017 study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (retain, nonatomic) UIWindow *window;
+
+
+@end
+
