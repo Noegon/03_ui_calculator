02083ad3a29555cce828b2ce5547d911a25ae193
Alexey Stafeyev <noegon@yandex.ru>
2017-05-21 19:30:09 +0300

    [HOTFIX]Outlets and actions were replaced into extension. And also some undefined conflict was handled

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -19,6 +19,19 @@ static NSInteger const maxAmountOfDigitsInInsertionField = 18;
 
 @property (retain, nonatomic) UIBarButtonItem *aboutBarButton;
 @property (retain, nonatomic) UIBarButtonItem *licenseBarButton;
+@property (retain, nonatomic) IBOutlet UILabel *digitInsertionField;
+@property (retain, nonatomic) IBOutletCollection(UIButton) NSArray *digitButtonsArray;
+@property (retain, nonatomic) IBOutlet UIButton *dotButton;
+@property (retain, nonatomic) IBOutlet UIButton *equalButton;
+@property (retain, nonatomic) IBOutlet UIButton *clearButton;
+@property (retain, nonatomic) IBOutlet UIButton *aboutButton;
+
+- (IBAction)digitButtonTouched:(UIButton *)sender;
+- (IBAction)clearButtonTouched:(UIButton *)sender;
+- (IBAction)handleSwipeGesture:(UISwipeGestureRecognizer *)sender;
+- (IBAction)aboutButtonTouched:(UIButton *)sender;
+- (IBAction)licenseButtonTouched:(UIBarButtonItem *)sender;
+- (IBAction)dotButtonTouched:(UIButton *)sender;
 
 - (void)switchCalculationButtonsEnabled:(BOOL)areButtonsEnabled;
 
