ad4fd733319ffa66ef5280b34db2624c8353899a
Alex Stafeyev <noegon@yandex.ru>
2017-07-20 13:26:12 +0300

    [HOTFIX]Names of CalcModel methods were corrected

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -110,55 +110,55 @@ static const double unchosenNotationButtonAlpha = 1.0;
     self.navigationItem.rightBarButtonItem = licenseBarButton;
     
     [self.model addUnaryOperationWithOperationSymbol:@"x²"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand *= currentOperand;
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"sin"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = sin(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"cos"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = cos(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"tg"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = tan(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"ctg"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = 1 / tan(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"e"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = M_E;
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"π"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = M_PI;
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"ln"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = log(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
     
     [self.model addUnaryOperationWithOperationSymbol:@"log"
-                                           WithBlock:^(double result, double currentOperand, Results *results){
+                                           block:^(double result, double currentOperand, Results *results){
                                                currentOperand = log10(currentOperand);
                                                results->currentOperand = currentOperand;
                                            }];
